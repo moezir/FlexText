75d65b413381dc17f47feef10d28cce6e459f30d
Motasem Zir <moe.zeer@gmail.com>
2019-02-06 15:58:13 +0200

    Update flextext.js

--- flextext.js
+++ flextext.js
@@ -1,14 +1,14 @@
-﻿/**
- * @preserve  flextext
- * @name      flextext.js
- * @author    Motasem Zir
- * @author    moe.zeer@gmail.com
- * @version   0.5.1
- * @date      2019-01-19
- * @copyright (c) 2019
- * @license   Apache License 2.0
- * @homepage  https://github.com/moezir/FlexText
- */
+/**
+* @preserve  flextext
+* @name      flextext.js
+* @author    Motasem Zir
+* @author    moe.zeer@gmail.com
+* @version   0.5.1
+* @date      2019-01-19
+* @copyright (c) 2019
+* @license   Apache License 2.0
+* @homepage  https://github.com/moezir/FlexText
+*/
 
 (function ($) {
     $.fn.flextext = function (options) {
@@ -20,7 +20,7 @@
             marginV: "0px",
             marginH: "0px",
             fontWeight: "normal",
-            onSuccess: function() {}
+            onSuccess: function () { }
         }, options);
 
         $this = this;
@@ -45,19 +45,21 @@
         function ft_Flex(el) {
             return $(el).each(function () {
                 $(this).css(_overflow, _hidden);
-                $(this).css(_padding, '0'); 
-                $(this).css(_margin, _settings.marginV + ' ' + _settings.marginH);            
+                $(this).css(_padding, '0');
+                $(this).css(_margin, _settings.marginV + ' ' + _settings.marginH);
                 $(this).css(_fontWeight, _settings.fontWeight);
-                
+                var isSuccess = false;
                 if (_settings.oneLine)
                     $(this).css(_whiteSpace, _nowrap);
                 for (var i = _settings.maxFont; i >= _settings.minFont; i--) {
                     $(this).css(_fontSize, i + "px");
                     if ((Math.ceil($(this).prop(_scrollWidth)) <= Math.ceil($(this).width()) && Math.ceil($(this).prop(_scrollHeight)) <= Math.ceil($(this).height())) || i == _settings.minFont) {
+                        isSuccess = true;
                         break;
                     }
-                    _settings.onSuccess.call(this);
                 }
+                if (isSuccess)
+                    _settings.onSuccess.call($(this));
             });
         }
 
